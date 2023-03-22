a812d4c367f956e0ddc4ae142f4dab68648cf3f7
Akash Darwin <akash.20211377@iit.ac.lk>
2023-03-22 09:22:25 +0530

    final model, with 82% accuracy holding 23 local snake species

--- model/model.py
+++ model/model.py
@@ -6,17 +6,12 @@ from keras.applications.inception_v3 import InceptionV3
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.models import Model
 from keras import regularizers
-from keras.callbacks import EarlyStopping
 
-# Load and preprocess data
-train_df = pd.read_csv('dataset/Csv/train.csv')
-test_df = pd.read_csv('dataset/Csv/test.csv')
+train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
-train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, rotation_range=20, vertical_flip=True)
-test_datagen = ImageDataGenerator(rescale=1./255, rotation_range=20, vertical_flip=True)
-
-train_generator = train_datagen.flow_from_directory('dataset/train/', target_size=(224, 224), batch_size=32, class_mode='categorical')
-test_generator = test_datagen.flow_from_directory('dataset/test/', target_size=(224, 224), batch_size=32, class_mode='categorical')
+train_generator = train_datagen.flow_from_directory('dataset_lankan5species - Copy (2)/train/', target_size=(224, 224), batch_size=32, class_mode='categorical')
+test_generator = test_datagen.flow_from_directory('dataset_lankan5species - Copy (2)/test/', target_size=(224, 224), batch_size=32, class_mode='categorical')
 
 # Load pre-trained model
 base_model = InceptionV3(weights='imagenet', include_top=False)
@@ -25,8 +20,9 @@ base_model = InceptionV3(weights='imagenet', include_top=False)
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 x = Dense(1024, activation='relu', kernel_regularizer=regularizers.l2(0.01))(x)
-x = Dense(1024, activation='relu', kernel_regularizer=regularizers.l2(0.01))(x)
-predictions = Dense(135, activation='softmax')(x)
+x = Dense(512, activation='relu', kernel_regularizer=regularizers.l2(0.01))(x) # Added layer
+x = Dense(256, activation='relu', kernel_regularizer=regularizers.l2(0.01))(x) # Added layer
+predictions = Dense(23, activation='softmax')(x)
 
 model = Model(inputs=base_model.input, outputs=predictions)
 
@@ -42,24 +38,18 @@ adam = tf.keras.optimizers.Adam(learning_rate=0.0001)
 # Compile model
 model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Early stopping
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
-
 # Train model
-history = model.fit(train_generator, epochs=50, validation_data=test_generator, callbacks=[es])
+history = model.fit(train_generator, epochs=30, validation_data=test_generator)
 
 # Print accuracy
 print('Final training accuracy:', history.history['accuracy'][-1])
 print('Final validation accuracy:', history.history['val_accuracy'][-1])
 
-#continue only the rest of the code Evaluate model on test data
-test_loss, test_acc = model.evaluate(test_generator)
-print('Test accuracy:', test_acc)
-
 # Save model
-model.save('model.h5')
+model.save('my_model2_two_inception3withbigaugmentation.h5')
 
 # Plot training and validation accuracy
+
 import matplotlib.pyplot as plt
 
 acc = history.history['accuracy']
