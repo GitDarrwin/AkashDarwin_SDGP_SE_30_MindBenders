ab8dc4a230fc4dbd6d4837f86dbc0da09d71d9c8
Akash Darwin <akash.20211377@iit.ac.lk>
2023-03-23 11:32:44 +0530

    model tester with images

---
+++ model/testmodel.py
@@ -0,0 +1,21 @@
+import numpy as np
+from keras.models import load_model
+from keras.utils import load_img, img_to_array
+
+# Load saved model
+model = load_model('my_model2_two_inception3withbigaugmentation.h5')
+
+# Load and preprocess image
+img = load_img('1676200308703__1_-removebg-preview.png', target_size=(224, 224))
+img_array = img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+img_array /= 255.
+
+# Make prediction
+prediction = model.predict(img_array)
+
+# Print predicted class and confidence level
+predicted_class = np.argmax(prediction)
+confidence_level = np.max(prediction)
+print('Predicted class:', predicted_class)
+print('Confidence level:', confidence_level)
