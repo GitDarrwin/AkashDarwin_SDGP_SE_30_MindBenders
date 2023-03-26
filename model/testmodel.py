e8c7aa97f2e66e6c87ca7fb3ef7367aeb5d85966
Akash Darwin <akash.20211377@iit.ac.lk>
2023-03-26 17:03:36 +0530

    tiny changes

--- model/testmodel.py
+++ model/testmodel.py
@@ -6,7 +6,7 @@ from keras.utils import load_img, img_to_array
 model = load_model('my_model2_two_inception3withbigaugmentation.h5')
 
 # Load and preprocess image
-img = load_img('1676200308703__1_-removebg-preview.png', target_size=(224, 224))
+img = load_img('Green_pit_viper.jpeg', target_size=(224, 224))
 img_array = img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
 img_array /= 255.
